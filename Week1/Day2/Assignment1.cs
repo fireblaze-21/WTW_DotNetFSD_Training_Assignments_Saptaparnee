8c1b543c00ed4c4be115b40f84b9f2c607f3c4ca
fireblaze_21 <saptaparnee1234@gmail.com>
2025-07-10 09:50:38 +0000

    commit

---
+++ Week1/Day2/Assignment1.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class configurationManager
+    {
+        
+        // Compile-time constant (fixed value)
+        public const int MaximumConnections = 100;
+
+        
+         // Runtime-initialized readonly variable
+    public  readonly string ApiKey;
+
+    
+     public configurationManager(string apiKey)
+    {
+        ApiKey = apiKey;
+    }
+    public void display()
+        {
+            Console.WriteLine($"Maximum Connections: {MaximumConnections}");
+           
+        }
+    }
+
+    class config
+    {
+
+        static void Main()
+        {
+
+            configurationManager obj1 = new configurationManager("ABC123");
+           
+             Console.WriteLine($"API Key: {obj1.ApiKey}");
+
+            Console.WriteLine("--------------------------------------");
+
+             configurationManager obj2 = new configurationManager("XYZ789");
+           
+             Console.WriteLine($"API Key: {obj2.ApiKey}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week1/Day2/Assignment2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+     class EmployeeDatabase
+    {
+
+        static void DisplayResult( int employeeId,string employeeName, string employeeJob, string? terminationDate = default)
+        {
+            string name = employeeName;
+            int id = employeeId;
+            string job = employeeJob;
+            
+            string displayDate = terminationDate ?? "";    
+
+            Console.WriteLine($"Employee-ID: {id}");
+            Console.WriteLine($"Employee-Name: {name}");
+            Console.WriteLine($"Employee-Job: {job}");
+
+
+
+            if (!string.IsNullOrEmpty(displayDate))
+            {
+                Console.WriteLine($"Employee Termination Date : {displayDate}");
+            }
+            else
+            {
+                Console.WriteLine("Active Employee.");
+            }
+
+            Console.WriteLine("------------------------------");
+
+        }
+        
+
+        static void Main()
+        {
+
+            DisplayResult(1,"Scott","CEO", "1.1.2025");
+            DisplayResult(2,"Smith","Analyst","");
+            DisplayResult(3,"Sam","Manager" ,"3.6.2025");
+            DisplayResult(4,"Bob","Teacher","");
+           
+            Console.ReadLine();
+        }
+    }
+}
