32d258de89d7676e0fd501c8f144cabbf1d595e9
fireblaze_21 <saptaparnee1234@gmail.com>
2025-07-11 09:20:29 +0000

    commit

---
+++ Week1/Day3/AccessScore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class AccessScore
+    {
+        public int UserLevelScore;
+        public bool IsAdmin;
+        public bool HasSpecialPermission;
+        public string UserName;
+
+        public int CalculateAccessScore()
+        {
+            int accessScore = 0;
+
+            if (IsAdmin == true)
+            {
+                accessScore += 50;
+            }
+            if (HasSpecialPermission == true)
+            {
+                accessScore += 25;
+            }
+            accessScore += UserLevelScore;
+            return accessScore;
+        }
+
+        public void DisplayAccessControlStatus(int accessScore)
+        {
+            Console.WriteLine($" User Name :  {UserName}");
+            Console.WriteLine($" User Level Score :  {UserLevelScore} ");
+            Console.WriteLine($" User Access Score :  {accessScore} ");
+            if (accessScore < 25)
+            {
+                Console.WriteLine($" Access Control Status : Restricted Access");
+            }
+            else if (accessScore >= 25 && accessScore < 50)
+            {
+                Console.WriteLine($"  Access Control Status : Standard Access");
+            }
+            else if (accessScore >= 50 && accessScore < 75)
+            {
+                Console.WriteLine($" Access Control Status :  Admin Access");
+            }
+            else
+            {
+                Console.WriteLine($"  Access Control Status : Full Access");
+            }
+            
+        }
+    
+    }
+}
---
+++ Week1/Day3/StatusDisplay.cs
@@ -0,0 +1,46 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Day3
+{
+    class StatusDisplay
+    {
+        static void Main()
+        {
+            int userScore;
+            string userName;
+            AccessScore obj = new AccessScore();
+            Console.WriteLine("Enter User Name: ");
+            userName= Console.ReadLine();
+            Console.WriteLine("Enter User Score: ");
+            userScore= int.Parse(Console.ReadLine());
+            Console.WriteLine("Is User Admin (Enter true or false) :");
+            string input1 = Console.ReadLine();
+            bool admin = bool.Parse(input1);
+            Console.WriteLine("Has User Special Permission (Enter true or false) :");
+            string input2 = Console.ReadLine();
+            bool permission = bool.Parse(input2);
+            obj.UserName = userName;
+            obj.UserLevelScore = userScore;
+            obj.IsAdmin = admin;
+            obj.HasSpecialPermission = permission;
+
+            if (userScore < 1 || userScore > 25)
+            {
+                Console.WriteLine("Invalid User Score");
+                Console.ReadLine();
+                return;
+            }
+
+            int calAccessScore = obj.CalculateAccessScore();
+            obj.DisplayAccessControlStatus(calAccessScore);
+
+
+            Console.ReadLine();
+        }
+    }
+
+}
