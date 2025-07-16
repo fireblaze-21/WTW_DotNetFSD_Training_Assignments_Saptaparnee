6392edf761b71f3297f9428cad1d1d49e6a713f7
fireblaze-21 <saptaparnee1234@gmail.com>
2025-07-16 15:26:26 +0530

    Upload

---
+++ Week2/Day3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string AccountHolder { get; set; }
+        public double Balance { get; set; }
+       
+
+        public BankAccount(string accountNumber, string accountHolder, double balance)
+        {
+            this.AccountNumber = accountNumber;
+            this.AccountHolder = accountHolder;
+            this.Balance = balance > 0 ? balance : 0; 
+        }
+
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine($"----------------------Account Details-----------------");
+            Console.WriteLine($" Account Holder Name : {AccountHolder}");
+            Console.WriteLine($" Account Number : {AccountNumber}");
+            Console.WriteLine($" Balance INR : {Balance}");
+
+        }
+    }
+    class BankAccountManagement
+    {
+        // Deposit Amount
+        public static void Deposit(BankAccount bankAccount , double depositAmount)
+        { 
+            if(depositAmount<0)
+            {
+                Console.WriteLine($"Invalid Deposit Amount. ");
+                return;
+            }
+            bankAccount.Balance += depositAmount;
+        }
+
+        // Withdraw Amount
+        public static void Withdraw(BankAccount bankAccount , double withdrawAmount)
+        {
+            if(withdrawAmount<0)
+            {
+                Console.WriteLine($"Invalid Deposit Amount.");
+                return;
+            }
+            if(bankAccount.Balance<withdrawAmount)
+            {
+                Console.WriteLine($" Withdrawal Amount Exceeds Current Balance. ");
+                return;
+            }
+            bankAccount.Balance -= withdrawAmount;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankaccount = new BankAccount("12345", "Narasimha", 1000);
+            
+            bankaccount.DisplayAccountInfo();
+            Console.WriteLine($"-------------------Deposit INR 500-----------------------");
+            BankAccountManagement.Deposit(bankaccount, 500);
+
+            bankaccount.DisplayAccountInfo();
+            Console.WriteLine($"--------------------Withdraw INR 200---------------------");
+            BankAccountManagement.Withdraw(bankaccount, 200);
+
+            bankaccount.DisplayAccountInfo();
+            Console.WriteLine($"--------------------Withdraw INR 2000---------------------");
+            BankAccountManagement.Withdraw(bankaccount, 2000);
+
+            bankaccount.DisplayAccountInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week2/Day3/Program2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+
+        public Book (string title, string author, string isbn)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.ISBN = isbn;
+            this.IsAvailable = true;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"----------------------Book Details-----------------");
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine("ISBN: " + ISBN);
+            Console.WriteLine("Available: " + (IsAvailable ? "Yes" : "No"));
+            Console.WriteLine("----------------------------------------------------");
+        }
+
+        public void BorrowBook()
+        {
+            if (IsAvailable)
+            {
+                IsAvailable = false;
+                Console.WriteLine($"Successfully borrowed \"{Title}\".");
+            }
+            else
+            {
+                Console.WriteLine($"The \"{Title}\" is currently not available.");
+            }
+        }
+
+    }
+    
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("1984", "George Orwell", "123456789");
+            Book book2 = new Book("Pride and Prejudice", "Jane Austen", "987654321");
+
+            book1.DisplayInfo();
+            book2.DisplayInfo();
+
+            Console.WriteLine($"-------------------Borrow first book-----------------------");
+            book1.BorrowBook();
+            book1.DisplayInfo();
+            Console.WriteLine($"-------------------Borrow second book-----------------------");
+            book2.BorrowBook();
+            book2.DisplayInfo();
+            Console.WriteLine($"-------------------Borrow first book AGAIN-----------------------");
+            book1.BorrowBook();
+            book1.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
+
